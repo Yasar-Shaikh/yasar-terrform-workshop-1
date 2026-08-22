8df7990b2e1a4fbb211b1394fee1c0c5efb6ea53
Yasar Saikh <yasarshaikh17@gmail.com>
2026-08-22 12:48:05 +0530

    test

--- main.tf
+++ main.tf
@@ -62,4 +62,7 @@ resource "azurerm_network_security_rule" "rdp" {
   resource_group_name         = azurerm_resource_group.rg.name
   network_security_group_name = azurerm_network_security_group.nsg.name
 }
+#
+
+
 
