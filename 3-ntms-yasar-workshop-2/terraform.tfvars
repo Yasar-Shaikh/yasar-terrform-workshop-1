9c4d65a7eb1ea420e8e3d5ae7908c36eb38dedd1
Yasar Shaikh <Yasar.shaikh17@gmail.com>
2026-08-29 15:02:37 +0530

    test

---
+++ 3-ntms-yasar-workshop-2/terraform.tfvars
@@ -0,0 +1,7 @@
+BU = "IT-infra"
+env = "staging"
+location = "West US"
+owner = "yasar"
+subnet_address_prefixes = [ "10.1.1.0/24" ]
+vnet_address_space = [ "10.1.0.0/16" ]
+
--- 3-ntms-yasar-workshop-2/var.tf
+++ 3-ntms-yasar-workshop-2/var.tf
@@ -1,14 +1,14 @@
 variable "BU" {
     description = "The name of the resource group"
     type        = string
-    default     = "IT-infra"
+    
   
 }
 
 variable "location" {
     description = "The location of the resource group"
     type        = string
-    default     = "West US"
+   
   
 }
 
@@ -17,7 +17,7 @@ variable "location" {
 variable "vnet_address_space" {
     description = "The address space of the virtual network"
     type        = list(string)
-    default     = ["10.1.0.0/16"]
+   
 
 }
 
@@ -25,7 +25,7 @@ variable "vnet_address_space" {
 variable "subnet_address_prefixes" {
     description = "The address prefixes of the subnet"
     type        = list(string)
-    default     = ["10.1.1.0/24"]
+  
 
 }
 
@@ -34,11 +34,11 @@ variable "subnet_address_prefixes" {
 variable "owner" {
     description = "The name of the HTTP security rule"
     type        = string
-    default     = "yasar"
+   
  }
 
 variable "env" {
     description = ""
     type        = string
-    default     = "dev"
+   
  }
